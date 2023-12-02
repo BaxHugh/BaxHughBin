3724db2c1a890d9e19d3f519704198d782396129
Hugh <hughdbaxter@gmail.com>
2023-12-02 14:57:25 +0000

    copy-iphone-photos.sh updated to align with iOS 17 (no DCIM folder anymore)

--- copy-iphone-photos.sh
+++ copy-iphone-photos.sh
@@ -2,9 +2,10 @@
 
 Help() {
     # Display Help
-    echo "Run rsync for iphone DCIM directory."
+    echo "Run rsync for iphone photo directory."
     echo
     echo "Usage: copy-iphone-photos.sh DCIM_COPY_DESTINATION"
+    echo "Assumes the iphone photo directory can be matched to '/run/user/1000/gvfs/gphoto2:host=Apple_Inc*'"
 }
 
 # Get the options
@@ -18,11 +19,12 @@ done
 
 GVFS="/run/user/1000/gvfs"
 # This assumes there's only one device at GVFS
-DCIM=$(cd $GVFS/*/DCIM && pwd)
+IPHONE_PHOTOS_PATH=$(cd $GVFS/gphoto2:host=Apple_Inc* && pwd)
 
-echo "Using DCIM path: $DCIM"
+echo "Using path: $IPHONE_PHOTOS_PATH"
 # Eclude live photo videos
-#rsync --ignore-existing --exclude IMG*.MOV --exclude *.AAE -r $DCIM $1 > ~/DCIM.log
-rsync --ignore-existing -r $DCIM $1 &> ~/DCIM.log
+#rsync --ignore-existing --exclude IMG*.MOV --exclude *.AAE -r $IPHONE_PHOTOS_PATH $1 > ~/iphone-photos-copy.log
+rsync --ignore-existing -r $IPHONE_PHOTOS_PATH/* $1 &> ~/iphone-photos-copy.log
+cat ~/iphone-photos-copy.log
 
-echo "Done. See ~/DCIM.log for errors and files which weren't copied."
+echo "Done. See ~/iphone-photos-copy.log for errors and files which weren't copied."
